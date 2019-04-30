0ec9f87e4a1b3c7283f28d2436c7097625839f4b
DcguentherATX <davidcarlguenther@gmail.com>
2019-04-30 17:13:03 -0500

    Getting Started on Promises

--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -10,8 +10,18 @@ var Promise = require('bluebird');
 
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
-  // TODO
-};
+  let promise = new Promise ((resolve, reject) => { 
+    fs.readFile(filePath, 'utf8', (err, singleFile) => {
+    if (err) {
+      reject(err); 
+    } else {
+      let firstLine = singleFile.split(`\n`)[0];
+      resolve(firstLine); 
+    }
+    });
+  });
+  return promise
+}
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
